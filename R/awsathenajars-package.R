fac06495b83b7d1f579adb0dc77138deef5cb1f1
boB Rudis <bob@rud.is>
2019-02-22 15:14:58 -0500

    initial commit

---
+++ R/awsathenajars-package.R
@@ -0,0 +1,10 @@
+# Java Archive Wrapper Supporting the 'awsathena' Package
+#'
+#' Contents of the ...
+#'
+#' @md
+#' @name awsathenajars
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import rJava
+NULL
