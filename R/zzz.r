e4b10063430fdec3081192ff998ca8fd9c4602e1
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-27 09:31:23 +0800

    Delete R directory

--- R/zzz.r
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
-  rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
-  stop_logging()
-}
